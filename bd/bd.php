5107c43b3b444ef6d7a4ebdc56a9a5887b4341cc
jonathanss <yonidefines@hotmail.com>
2022-02-23 11:30:38 +0100

    agrego proyecto

---
+++ bd/bd.php
@@ -0,0 +1,13 @@
+<!-- Clase con los datos de acceso a la base de datos -->
+<?php
+    class bd{
+        protected $bbdd = "concierto";    
+        protected $username = "root";
+        protected $password = "1234";
+        protected $conexion;
+
+        public function __construct(){
+            $this->conexion = new PDO('mysql:host=localhost;dbname='.$this->bbdd, $this->username, $this->password);
+        }
+    }
+?>
